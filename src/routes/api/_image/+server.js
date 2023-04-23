cb7a5a1940efb3a3934e475fb149d131d82e63b3
wyousefsaid <wasimysdev@proton.me>
2023-04-23 14:52:39 +0000

    this causing the build to fail hmmmmmm

--- src/routes/api/_image/+server.js
+++ src/routes/api/_image/+server.js
@@ -3,7 +3,7 @@ import { requestHandler } from 'sveltekit-image/api'
 export const GET = requestHandler({
   leadingUrl:
     process.env.NODE_ENV === 'production'
-      ? 'https://sveltekit-image.opensource.moonface.digital'
+      ? 'https://preeminent-banoffee-60b370.netlify.app'
       : 'http://127.0.0.1:5173',
 			ttl: 1000 * 60 * 60 * 24 * 7,
 			storePath: '.svelte-kit/images',
