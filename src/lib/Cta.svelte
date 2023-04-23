46b65d01bcec8da521467c1e33ba4bce145f4ab9
wyousefsaid <wasimysdev@proton.me>
2023-04-23 18:55:44 +0000

    removed 1 testemonial

--- src/lib/Cta.svelte
+++ src/lib/Cta.svelte
@@ -59,7 +59,7 @@ on:change={({ detail }) => {
 <!-- END Heading -->
 
     <!-- Feedback -->
-    <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 md:gap-8">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-8">
       <div class="group flex flex-col px-6 pt-8 pb-6 md:px-8 md:pb-8 relative  border border-gray-600 rounded-xl">
         <div class="absolute top-0 right-0 text-7xl mt-3 mr-4 text-gray-200 opacity-75 font-serif">“</div>
         <blockquote class="grow flex flex-col relative">
@@ -89,34 +89,7 @@ on:change={({ detail }) => {
           </footer>
         </blockquote>
       </div>
-      <div class="group flex flex-col items-stretch px-6 pt-8 pb-6 md:px-8 md:pb-8 relative  border border-gray-600 rounded-xl">
-        <div class="absolute top-0 right-0 text-7xl mt-3 mr-4 text-gray-200 opacity-75 font-serif">“</div>
-        <blockquote class="grow flex flex-col relative">
-          <div class="mb-6">
-            <div class="inline-flex items-center text-rose-600 py-1 rounded-lg transition ease-out origin-top-left ">
-              <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="hi-solid hi-star inline-block w-5 h-5"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-              <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="hi-solid hi-star inline-block w-5 h-5"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-              <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="hi-solid hi-star inline-block w-5 h-5"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-              <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="hi-solid hi-star inline-block w-5 h-5"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-              <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="hi-solid hi-star inline-block w-5 h-5"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-            </div>
-          </div>
-          <p class="grow text-sm text-gray-300 font-medium leading-relaxed mb-5">
-           Kollat lite närmare nu i dagsljuset, ser bra ut. Kanonjobb. Kommer rekommendera dig.
-          </p>
-          <footer class="flex-none flex items-center space-x-4">
-           
-            <div>
-              <div href="javascript:void(0)" class="font-semibold text-rose-600 hover:text-rose-400">
-                Jonas
-              </div>
-              <p class="text-sm text-gray-500 font-medium">
-                Kia Stinger
-              </p>
-            </div>
-          </footer>
-        </blockquote>
-      </div>
+      
       <div class="group flex flex-col items-stretch px-6 pt-8 pb-6 md:px-8 md:pb-8 relative  border border-gray-600 rounded-xl">
         <div class="absolute top-0 right-0 text-7xl mt-3 mr-4 text-gray-200 opacity-75 font-serif">“</div>
         <blockquote class="grow flex flex-col relative">
