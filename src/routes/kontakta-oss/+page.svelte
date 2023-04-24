356d44959564095af2a60ebe9ff712bfe8756172
wyousefsaid <wasimysdev@proton.me>
2023-04-24 10:42:34 +0000

    change names to swedish

--- src/routes/kontakta-oss/+page.svelte
+++ src/routes/kontakta-oss/+page.svelte
@@ -36,12 +36,12 @@
             <input type="hidden" name="form-name" value="contact" />
             <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
               <div class="space-y-1">
-                <label for="firstname" class="font-medium">First Name
-                <input class="block border placeholder-gray-490 px-5 bg-gray-900 py-3 leading-6 w-full rounded border-gray-200 focus:border-rose-500 focus:ring focus:ring-rose-500 focus:ring-opacity-50" type="text" name="name" id="name" required placeholder="Name" ></label>
+                <label for="firstname" class="font-medium">Förnamn
+                <input class="block border placeholder-gray-490 px-5 bg-gray-900 py-3 leading-6 w-full rounded border-gray-200 focus:border-rose-500 focus:ring focus:ring-rose-500 focus:ring-opacity-50" type="text" name="Förnamn" id="name" required placeholder="Förnamn" ></label>
               </div>
               <div class="space-y-1">
-                <label for="lastname" class="font-medium">Last Name
-                <input class="block border placeholder-gray-400 px-5 py-3 bg-gray-900 leading-6 w-full rounded border-gray-200 focus:border-rose-500 focus:ring focus:ring-rose-500 focus:ring-opacity-50"  type="text" name="name" id="name" required placeholder="Name"></label>
+                <label for="lastname" class="font-medium">Efternamn
+                <input class="block border placeholder-gray-400 px-5 py-3 bg-gray-900 leading-6 w-full rounded border-gray-200 focus:border-rose-500 focus:ring focus:ring-rose-500 focus:ring-opacity-50"  type="text" name="Efternamn" id="name" required placeholder="Efternamn"></label>
               </div>
             </div>
             <div class="space-y-1">
@@ -49,12 +49,12 @@
               <input class="block border placeholder-gray-400 px-5 py-3 bg-gray-900 leading-6 w-full rounded border-gray-200 focus:border-rose-500 focus:ring focus:ring-rose-500 focus:ring-opacity-50" type="email" name="email" id="email" required placeholder="Email" ></label>
             </div>
             <div class="space-y-1">
-              <label for="message" class="font-medium">Message
-              <textarea class="block border border-gray-200 rounded bg-gray-900 placeholder-gray-400 px-5 py-3 w-full focus:border-rose-500 focus:ring focus:ring-rose-500 focus:ring-opacity-50" name="message" id="message" required placeholder="Message"  rows="6"></textarea></label>
+              <label for="message" class="font-medium">Meddelande
+              <textarea class="block border border-gray-200 rounded bg-gray-900 placeholder-gray-400 px-5 py-3 w-full focus:border-rose-500 focus:ring focus:ring-rose-500 focus:ring-opacity-50" name="Meddelande" id="message" required placeholder="Meddelande"  rows="6"></textarea></label>
             </div>
             <button type="submit" class="inline-flex justify-center items-center space-x-2 border font-semibold focus:outline-none px-3 py-2 leading-6 rounded border-rose-700 bg-rose-700 text-white hover:text-white hover:bg-rose-800 hover:border-rose-800 focus:ring focus:ring-rose-500 focus:ring-opacity-50 active:bg-rose-700 active:border-rose-700">
               <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="opacity-50 hi-solid hi-paper-airplane inline-block w-5 h-5"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
-              <span>Send Message</span>
+              <span>Skicka Email</span>
             </button>
           </form>
         </div>
