21b3fd4cce0955ba7459df492a3c59d7015a33cd
wyousefsaid <wasimysdev@proton.me>
2023-04-25 09:13:37 +0000

    back to  netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -6,7 +6,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			runtime: 'edge',
+			edge: true,
 		})
 	},
 	vitePlugin: {
